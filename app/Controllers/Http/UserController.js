56893f5a67e6507fe179c61f8b9ad8821594dc47
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2021-01-14 14:07:26 -0300

    create controllers user and sessions

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+  async index ({response}){
+    try {
+      const users = User.all()
+
+      return (users)
+    } catch (error) {
+      return response.status(error.status).send({error: 'Ops! Ocorreu um erro ao listar os usuários'});
+    }
+  }
+
+  async store ({request, response}){
+    try {
+      const data = request.only(['username', 'email', 'password'])
+
+      const user = await User.create(data)
+
+      return user
+    } catch (error) {
+      return response.status(error.status).send({ error: { message: 'Ops! Ocorreu um erro ao Criar este usuários!'}})
+    }
+  }
+}
+
+module.exports = UserController
