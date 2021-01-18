16ad242511adcd42c3bdf55b5b286f418f9e86e0
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2021-01-18 13:44:48 -0300

    route delete products

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -27,6 +27,17 @@ class ProductController {
         return response.status(error.status).send({ error: { message: 'Ops! Ocorreu um erro ao cadastrar transação'}})
     }
   }
+
+  async destroy({}){
+    try {
+      const product = await Product.findOrFail(params.id)
+
+      await product.delete()
+      return response.status(200).send({ message: 'Produto removido com sucesso!'})
+    } catch (error) {
+      return response.status(error.status).send({ error: { message: 'Ops! Ocorreu um erro ao remover esta transação'}})
+    }
+  }
 }
 
 module.exports = ProductController
--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,12 +1,18 @@
 'use strict'
-
+const User = use('App/Models/User')
 class SessionController {
   async store ({ request, auth}){
     const { identifier, password } = request.all()
 
     const token = await auth.attempt(identifier, password)
 
-    return token
+    const user = await User.query()
+    .where('identifier', identifier)
+    .with('user')
+    .select('id','username','identifier')
+    .fetch()
+
+    return {token, user}
   }
 }
 
