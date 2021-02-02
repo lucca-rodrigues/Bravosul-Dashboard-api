e32e89c365a2e7be3f423aff96edb228e54e434f
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2021-02-02 13:22:02 -0300

    refeactory products from user auth

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -18,20 +18,8 @@ class ProductController {
 
   async myProducts({response, params, auth}){
     try {
-      const id = params.id;
-
-      const user = await User.query(id)
-
-      .where('id', id)
-      .select('id','username','identifier')
-      .fetch()
-
-      if(auth.user.id != params.id){
-        return response.status(401).send({ error: 'Not authorized' })
-      }else{
-        const products = await Database.from('products').where('user_id', id).select('id','user_id','title', 'description', 'enabled')
-        return {user, products}
-      }
+      const product = await Product.query().where('user_id', auth.user.id).fetch()
+      return product
     } catch (error) {
       return response.status(error.status).send({ error: { message: 'Ops! Ocorreu um erro ao exibir os produtos deste usuário'}})
     }
